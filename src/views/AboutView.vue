4c7f6f9574b7ad2524c6e95b51e54c2c0439160d
xinyuan <13837120071@163.com>
2023-01-04 22:08:39 +0800

    初始化

---
+++ src/views/AboutView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div id="main" style="width: 400px; height: 300px"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'homeView',
+  data() {
+    return {};
+  },
+  mounted() {
+    this.myEcharts();
+  },
+  methods: {
+    myEcharts() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById('main'));
+
+      // 指定图表的配置项和数据
+      let option = {
+        animation: false,
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    }
+  }
+};
+</script>
